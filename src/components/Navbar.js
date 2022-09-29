76e9e399863679ad657212260620b7b2571561c4
alifouani1 <fouaniali08@gmail.com>
2022-09-29 13:52:27 +0300

    My first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+import React, { Component }  from 'react';
+import "../styles/Navbar.css";
+//import navlink
+// import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1>SunX</h1>
+        </Link>
+        <nav>
+          <div className="nav-links">
+            <Link to="/home">Home</Link>
+            {/* <Link to="/categories">Categories</Link> */}
+            {/* <Link>
+              <DropdownButton
+                id="dropdown-basic-button"
+                title="Dropdown button"
+              >
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </DropdownButton>
+            </Link> */}
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact Us</Link>
+            <Link to="/login" className="login">
+              Login
+            </Link>
+            <Link to="/signup" className="signup">
+              Signup
+            </Link>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
