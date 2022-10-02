f585c4457375c6768ccdf2f31c503335eddd2cb3
alifouani1 <fouaniali08@gmail.com>
2022-10-02 11:38:18 +0300

    home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,14 +1,88 @@
 import React from "react";
 import "../styles/Home.css";
+// import AnimatedText from "react-animated-text-content";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
+
+// const slideImages = [
+//   {
+//     url: "../webb-imgs/auroras.jpg",
+//     caption: "Slide 1",
+//   },
+//   {
+//     url: "../webb-imgs/auroras.jpg",
+//     caption: "Slide 2",
+//   },
+//   {
+//     url: "../webb-imgs/auroras.jpg",
+//     caption: "Slide 3",
+//   },
+// ];
+
+const slideImages = [
+  "../webb-imgs/auroras.jpg",
+  "https://react-slideshow.herokuapp.com/images/slide_3.jpg",
+  "https://react-slideshow.herokuapp.com/images/slide_4.jpg",
+];
+
+const properties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  arrows: true,
+  pauseOnHover: true,
+  onChange: (oldIndex, newIndex) => {
+    console.log(`slide transition from ${oldIndex} to ${newIndex}`);
+  },
+};
 
 const Home = () => {
   return (
     <div className="home">
-      <div className="section">
-        {/* <p className="section-1-text">
-            <span className="">What is JWST?</span>
-            <span className="section-1-text-2">The best place to learn about the Sun</span>
-        </p> */}
+      {/* <AnimatedText
+        type="words" // animate words or chars
+        animation={{
+          x: "200px",
+          y: "-20px",
+          scale: 1.1,
+          ease: "ease-in-out",
+        }}
+        animationType="float"
+        interval={0.06}
+        duration={0.8}
+        tag="p"
+        className="animated-paragraph"
+        includeWhiteSpaces
+        threshold={0.1}
+        rootMargin="20%"
+      >
+        Welcome to the JWST Guide?
+      </AnimatedText>
+      <AnimatedText
+        type="words" // animate words or chars
+        animation={{
+          x: "200px",
+          y: "-20px",
+          scale: 1.1,
+          ease: "ease-in-out",
+        }}
+        animationType="float"
+        interval={0.06}
+        duration={0.8}
+        tag="p"
+        className="animated-paragraph"
+        includeWhiteSpaces
+        threshold={0.1}
+        rootMargin="20%"
+      >
+        The James Webb Space Telescope (JWST) is the largest, most powerful, and
+        most complex space science telescope ever built. Webb will solve
+        mysteries in our solar system, look beyond to distant worlds around
+        other stars, and probe the mysterious structures and origins of our
+        universe and our place in it.
+      </AnimatedText> */}
+      {/* <div className="section">
         <div className="section-text">
           <h1>
             <span className="title">What is JWST?</span>
@@ -22,6 +96,7 @@ const Home = () => {
           </p>
         </div>
         <div className="section-1-img">
+<<<<<<< Updated upstream
           <div className="img">
             {/* <img
               src={require("../webb-imgs/auroras.jpg")}
@@ -30,6 +105,60 @@ const Home = () => {
             /> */}
           </div>
         </div>
+=======
+          <div className="slide-container">
+            <Slide>
+              {slideImages.map((slideImage, index) => (
+                <div className="each-slide" key={index}>
+                  <div style={{ backgroundImage: `url(${slideImage.url})` }}>
+                    <span>{slideImage.caption}</span>
+                  </div>
+                </div>
+              ))}
+            </Slide>
+          </div>
+        </div>
+      </div> */}
+      <div className="section">
+        <div className="section-text">
+          <h1>
+            <span className="title">What is JWST?</span>
+          </h1>
+          <p className="desc">
+            The James Webb Space Telescope (JWST) is the largest, most powerful,
+            and most complex space science telescope ever built. Webb will solve
+            mysteries in our solar system, look beyond to distant worlds around
+            other stars, and probe the mysterious structures and origins of our
+            universe and our place in it.
+          </p>
+        </div>
+        <div className="slide-container">
+          <Slide {...properties}>
+            <div className="each-slide">
+              <div style={{ backgroundImage: `url(${slideImages[0]})` }}>
+                {/* <span>Slide 1</span> */}
+                <img src={require("../webb-imgs/jwst1.jpg")} alt="auroras" />
+              </div>
+            </div>
+            <div className="each-slide">
+              <div style={{ backgroundImage: `url(${slideImages[1]})` }}>
+                <img src={require("../webb-imgs/jwst2.jpg")} alt="auroras" />
+
+              </div>
+            </div>
+            <div className="each-slide">
+              <div style={{ backgroundImage: `url(${slideImages[2]})` }}>
+                <img src={require("../webb-imgs/jwst3.jpg")} alt="auroras" />
+              </div>
+            </div>
+            <div className="each-slide">
+              <div style={{ backgroundImage: `url(${slideImages[2]})` }}>
+                <img src={require("../webb-imgs/jwst4.jpg")} alt="auroras" />
+              </div>
+            </div>
+          </Slide>
+        </div>
+>>>>>>> Stashed changes
       </div>
     </div>
   );
--- src/screens/img-category.js
+++ src/screens/img-category.js
@@ -6,53 +6,95 @@ import "../styles/imgCat.css";
 let slides = [
   //get imgs from imgs folder
   <img
-    src="https://picsum.photos/800/300/?random"
+    src={require("../webb-imgs/auroras.jpg")}
     alt="1"
-    width={500}
-    height={300}
+    width={550}
+    height={350}
   />,
   <img
-    src="https://picsum.photos/800/301/?random"
+    src={require("../webb-imgs/cosmicTarantula-2.jpg")}
     alt="2"
-    width={500}
-    height={300}
+    width={550}
+    height={350}
   />,
   <img
-    src="https://picsum.photos/800/302/?random"
+    src={require("../webb-imgs/distantWorld.jpg")}
     alt="3"
-    width={500}
-    height={300}
+    width={550}
+    height={350}
   />,
   <img
-    src="https://picsum.photos/800/303/?random"
+    src={require("../webb-imgs/IC5332.jpg")}
     alt="4"
-    width={500}
-    height={300}
+    width={550}
+    height={350}
   />,
   <img
-    src="https://picsum.photos/800/304/?random"
+    src={require("../webb-imgs/neptuneRing-2.jpg")}
     alt="5"
-    width={500}
-    height={300}
+    width={550}
+    height={350}
   />,
   <img
-    src="../imgs/webb-imgs/auroras.jpg"
+    src={require("../webb-imgs/neptuneRing-3.jpg")}
     alt="6"
-    width={500}
-    height={300}
+    width={550}
+    height={350}
   />,
   <img
-    src="https://picsum.photos/800/303/?random"
+    src={require("../webb-imgs/neptuneRing.jpg")}
     alt="7"
-    width={500}
-    height={300}
+    width={550}
+    height={350}
   />,
   <img
-    src="https://picsum.photos/800/303/?random"
+    src={require("../webb-imgs/phantomAcrossTheSpectrum.jpg")}
     alt="8"
-    width={500}
-    height={300}
+    width={550}
+    height={350}
   />,
+  <img
+    src={require("../webb-imgs/phantomGalaxy.jpg")}
+    alt="9"
+    width={550}
+    height={350}
+  />,
+  // <img
+  //   src={require("../webb-imgs/hubandWebb.jpg")}
+  //   alt="10"
+  //   width={550}
+  //   height={350}
+  // />,
+  // <img
+  //   src={require("../webb-imgs/auroras-1.jpg")}
+  //   alt="11"
+  //   width={550}
+  //   height={350}
+  // />,
+  // <img
+  //   src={require("../webb-imgs/blackholes-1.jpg")}
+  //   alt="12"
+  //   width={550}
+  //   height={350}
+  // />,
+  // <img
+  //   src={require("../webb-imgs/dryingstars.jpg")}
+  //   alt="13"
+  //   width={550}
+  //   height={350}
+  // />,
+  // <img
+  //   src={require("../webb-imgs/infrared.jpg")}
+  //   alt="14"
+  //   width={550}
+  //   height={350}
+  // />,
+  // <img
+  //   src={require("../webb-imgs/nebula.jpg")}
+  //   alt="15"
+  //   width={550}
+  //   height={350}
+  // />,
 ];
 
 export const ImgCategory = () => {
